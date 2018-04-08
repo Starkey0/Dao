217f799c2b0ad4a689a79f30397dc1252df64bf3
Sebastien Neron <neron.seb@gmail.com>
2018-04-08 13:55:25 -0400

    Initial commit

---
+++ Assets/Scripts/UserControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserControl : MonoBehaviour {
+
+    private Character m_Character;
+    private bool m_Jump;
+    private bool crouch;
+    private bool moveQ, moveE, moveS;
+
+
+
+
+    private void Awake()
+    {
+        m_Character = GetComponent<Character>();
+        crouch = false;
+    }
+
+
+    private void Update()
+    {
+        // Read the jump input in Update so button presses aren't missed.
+        m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = !crouch;
+        }
+        moveQ = Input.GetButtonDown("Q");
+        moveE = Input.GetButtonDown("E");
+        moveS = Input.GetButtonDown("S");
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        // Read the inputs.
+
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        m_Character.Move(h, crouch, m_Jump, moveQ, moveE, moveS);
+        m_Jump = false;
+    }
+}
